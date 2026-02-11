87c5680b11206e085484d3661295fb1f5b1b5dd5
Sacha Laville <sacha_laville@outlook.fr>
2026-02-12 00:33:34 +0100

    update: ajout d'un systeme qui ne change que le contenu a afficher (hors navbar) dans le dashboard.html

--- public/src/scripts/script.js
+++ public/src/scripts/script.js
@@ -1,4 +1,37 @@
+// navbar du dashboard
 
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const container = document.getElementById('monitoring-container');
+
+    async function loadPage(url) {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) throw new Error("Erreur de chargement");
+
+            const html = await response.text();
+            container.innerHTML = html;
+        } catch (error) {
+            console.error(error);
+            container.innerHTML = "<p style='color:white;'>Erreur : Impossible de charger la vue.</p>";
+        }
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            navLinks.forEach(l => l.removeAttribute('id'));
+            link.setAttribute('id', 'dashboard-active-page');
+            navLinks.forEach(l => l.removeAttribute('id'));
+            link.setAttribute('id', 'dashboard-active-page');
+
+            const pageToLoad = link.getAttribute('data-page');
+            loadPage(pageToLoad);
+        });
+    });
+    loadPage('views/monitoring.html');
+});
 
 // navbar re-sizing to scroll
 
@@ -12,7 +45,7 @@ window.addEventListener('scroll', () => {
     }
 })
 
-//
+// menu hamburger
 
 const change = document.querySelector('#hamburgerBtn');
 const menu = document.querySelector('#mobile');
